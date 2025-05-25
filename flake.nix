f1c5fd2e71145c21278fe9e44bc90708429d24a1
Justin Hoang <j124.dev@proton.me>
2025-05-25 11:28:52 -0600

    refactor: revert to simple flake template

--- flake.nix
+++ flake.nix
@@ -2,67 +2,49 @@
   description = "suasuasuasuasua's nixvim config";
 
   inputs = {
-    # main
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    nixvim.url = "github:nix-community/nixvim";
-
-    # utility
+    # use the latest stable branch
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixvim.url = "github:nix-community/nixvim/nixos-25.05";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    systems.url = "github:nix-systems/default";
   };
 
   outputs =
-    {
-      self,
-      nixvim,
-      systems,
-      flake-parts,
-      ...
-    }@inputs:
-    # TODO: move away from flake-parts
-    # I think it's super convenient, but I don't like the idea of tying myself
-    # to some nix framework that is still experimental. Ideally, I would just
-    # implement this all in native nix and flake module syntax. I am going with
-    # it now to move fast since it was included in the template
+    { nixvim, flake-parts, ... }@inputs:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = import systems;
-
-      imports = [
-        # Import nixvim's flake-parts module;
-        # Adds `flake.nixvimModules` and `perSystem.nixvimConfigurations`
-        nixvim.flakeModules.default
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
       ];
 
-      nixvim = {
-        # Automatically install corresponding packages for each nixvimConfiguration
-        # Lets you run `nix run .#<name>`, or simply `nix run` if you have a default
-        packages.enable = true;
-        # Automatically install checks for each nixvimConfiguration
-        # Run `nix flake check` to verify that your config is not broken
-        checks.enable = true;
-      };
-
-      # You can define your reusable Nixvim modules here
-      flake = {
-        nixosModules.default = import ./config;
-        darwinModules.default = import ./config;
-
-        nixvimModules = {
-          default = ./config;
-        };
-      };
-
+      # https://github.com/nix-community/nixvim/blob/1c5c991fda4519db56c30c9d75ba29ba7097af83/templates/simple/flake.nix
       perSystem =
         { pkgs, system, ... }:
+        let
+          nixvimLib = nixvim.lib.${system};
+          nixvim' = nixvim.legacyPackages.${system};
+          nixvimModule = {
+            inherit system; # or alternatively, set `pkgs`
+            module = import ./config; # import the module directly
+            # You can use `extraSpecialArgs` to pass additional arguments to your module files
+            extraSpecialArgs = {
+              # inherit (inputs) foo;
+            };
+          };
+          nvim = nixvim'.makeNixvimWithModule nixvimModule;
+        in
         {
           formatter = pkgs.nixfmt-rfc-style;
 
-          # You can define actual Nixvim configurations here
-          nixvimConfigurations = {
-            default = nixvim.lib.evalNixvim {
-              inherit system;
-              modules = [ self.nixvimModules.default ];
-            };
+          checks = {
+            # Run `nix flake check .` to verify that your config is not broken
+            default = nixvimLib.check.mkTestDerivationFromNixvimModule nixvimModule;
+          };
+
+          packages = {
+            # Lets you run `nix run .` to start nixvim
+            default = nvim;
           };
         };
     };
