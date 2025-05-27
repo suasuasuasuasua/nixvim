cd35fb565b3edc24f537a0d6445aaf1da07a7e30
Justin Hoang <j124.dev@proton.me>
2025-05-26 21:29:19 -0600

    docs(flake): update notes about flake-checker override

--- flake.nix
+++ flake.nix
@@ -69,9 +69,7 @@
             flake-checker = {
               enable = true; # run `flake check`
               # TODO: remove when v0.2.6 hits main nixpkgs
-              # 25.05 should be valid now.
-              # https://discourse.nixos.org/t/nixpkgs-overlay-for-mpd-discord-rpc-is-no-longer-working/59982/2
-              # https://discourse.nixos.org/t/how-do-you-override-the-commit-rev-used-by-a-rust-package/47698/6
+              # https://discourse.nixos.org/t/nixpkgs-overlay-for-mpd-discord-rpc-is-no-longer-working/59982
               package = pkgs.flake-checker.overrideAttrs rec {
                 pname = "flake-checker";
                 version = "0.2.6";
@@ -86,12 +84,6 @@
                   inherit src;
                   hash = "sha256-5eaVjrAPxBQdG+LQ6mQ/ZYAdslpdK3mrZ5Vbuwe3iQw=";
                 };
-                # NOTE: not working this way for some reason -- something about
-                # updating the cargoHash but that is never output
-                # cargoDeps = drv.cargoDeps.overrideAttrs {
-                #   inherit src;
-                #   hash = "sha256-5eaVjrAPxBQdG+LQ6mQ/ZYAdslpdK3mrZ5Vbuwe3iQw=";
-                # };
               };
             };
             statix.enable = true; # check "good practices" for nix
