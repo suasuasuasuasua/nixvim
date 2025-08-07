b71be6f1d0ae62b67cf9ad00bdaf9b2d484f6faf
Justin Hoang <j124.dev@proton.me>
2025-08-06 21:06:48 -0600

    feat(undotree): add new module
    useful for tracking changes to files like in vscode timeline

---
+++ config/plugins/custom/undotree.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  name = "undotree";
+  cfg = config.nixvim.plugins.custom.${name};
+in
+{
+  options.nixvim.plugins.custom.${name} = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable ${name} plugin for neovim";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # https://github.com/mbbill/undotree/
+    plugins.undotree = {
+      enable = true;
+
+      # https://nix-community.github.io/nixvim/plugins/undotree.html#pluginsundotreesettings
+      settings = { };
+    };
+  };
+}
