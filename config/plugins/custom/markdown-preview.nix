afcdca207da82da9f51e73212279fbacc630fd38
Justin Hoang <j124.dev@proton.me>
2025-08-08 11:08:36 -0600

    refactor(markdown-preview): use lazyLoad option instead of lz-n directly

--- config/plugins/custom/markdown-preview.nix
+++ config/plugins/custom/markdown-preview.nix
@@ -20,28 +20,20 @@ in
     # https://github.com/MeanderingProgrammer/render-markdown.nvim
     plugins.render-markdown = {
       enable = true;
-    };
 
-    plugins.lz-n = lib.mkIf config.plugins.lz-n.enable {
-      # https://nix-community.github.io/nixvim/plugins/lz-n/plugins.html
-      plugins = [
-        {
-          __unkeyed-1 = "render-markdown.nvim"; # the plugin's name (:h packadd)
-          after =
-            # lua
-            ''
-              function()
-                require('render-markdown').setup()
-              end
-            '';
+      lazyLoad = lib.mkIf config.plugins.lz-n.enable {
+        enable = true;
+
+        settings = {
+          event = [ "DeferredUIEnter" ];
           cmd = [
             "MarkdownPreviewToggle"
             "MarkdownPreview"
             "MarkdownPreviewStop"
           ];
           ft = "markdown";
-        }
-      ];
+        };
+      };
     };
   };
 }
