9a59e14f6dbcd2836fb3f7cd6f30639a071b2e74
Justin Hoang <j124.dev@proton.me>
2025-08-07 21:27:45 -0600

    feat(aerial): added more keymaps setup

--- config/plugins/custom/aerial.nix
+++ config/plugins/custom/aerial.nix
@@ -20,6 +20,35 @@ in
     # https://github.com/stevearc/aerial.nvim
     plugins.aerial = {
       enable = true;
+
+      luaConfig.post =
+        # lua
+        ''
+          require("aerial").setup({
+            -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+            on_attach = function(bufnr)
+              -- Jump forwards/backwards with '{' and '}'
+              vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+              vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+            end,
+          })
+        '';
+
+      lazyLoad = lib.mkIf config.plugins.lz-n.enable {
+        enable = true;
+
+        settings = {
+          event = [ "DeferredUIEnter" ];
+          keys = [
+            {
+              __unkeyed-1 = "<leader>a";
+              __unkeyed-3 = "<CMD>AerialToggle!<CR>";
+              mode = "n";
+              desc = "Toggle [A]erial";
+            }
+          ];
+        };
+      };
     };
   };
 }
