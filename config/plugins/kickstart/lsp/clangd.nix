2b86bf48ff1f078644760a47ba3f895711582062
Justin Hoang <j124.dev@proton.me>
2025-08-07 21:28:01 -0600

    refactor(lsp): move clangd to default enabled

--- config/plugins/kickstart/lsp/clangd.nix
+++ config/plugins/kickstart/lsp/clangd.nix
@@ -10,7 +10,11 @@ let
 in
 {
   options.nixvim.lsp.languages.${name} = {
-    enable = lib.mkEnableOption "Enable ${name} LSP for neovim";
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable ${name} LSP for neovim";
+    };
   };
 
   config = lib.mkIf cfg.enable {
