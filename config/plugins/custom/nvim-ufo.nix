347e9cef95fde4bd678968f403de4de30502edc3
Justin Hoang <j124.dev@proton.me>
2025-08-08 17:02:09 -0600

    refactor(nvim-ufo): move provider to config settings option

--- config/plugins/custom/nvim-ufo.nix
+++ config/plugins/custom/nvim-ufo.nix
@@ -21,19 +21,19 @@ in
       # https://github.com/kevinhwang91/nvim-ufo
       enable = true;
 
-      luaConfig.post =
-        # lua
-        ''
-          -- Option 3: treesitter as a main provider instead
-          -- (Note: the `nvim-treesitter` plugin is *not* needed.)
-          -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
-          -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
-          require('ufo').setup({
-            provider_selector = function(bufnr, filetype, buftype)
+      settings = {
+        provider_selector =
+          # lua
+          ''
+            -- Option 3: treesitter as a main provider instead
+            -- (Note: the `nvim-treesitter` plugin is *not* needed.)
+            -- ufo uses the same query files for folding (queries/<lang>/folds.scm)
+            -- performance and stability are better than `foldmethod=nvim_treesitter#foldexpr()`
+            function(bufnr, filetype, buftype)
               return {'treesitter', 'indent'}
             end
-          })
-        '';
+          '';
+      };
 
       lazyLoad = lib.mkIf config.plugins.lz-n.enable {
         enable = true;
