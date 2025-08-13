4ccd78801a29cf845bf4ef753dc40f6a122c83fe
Justin Hoang <j124.dev@proton.me>
2025-08-12 21:02:43 -0600

    refactor(lsp): disable harper by default for now
    hopefully should help performance
    maybe investigate other spell checkers

--- config/plugins/kickstart/lsp/extras/harper_ls.nix
+++ config/plugins/kickstart/lsp/extras/harper_ls.nix
@@ -9,11 +9,7 @@ let
 in
 {
   options.nixvim.lsp.languages.${name} = {
-    enable = lib.mkOption {
-      type = lib.types.bool;
-      default = true;
-      description = "Enable ${name} LSP for neovim";
-    };
+    enable = lib.mkEnableOption "Enable ${name} LSP for neovim";
   };
 
   config = lib.mkIf cfg.enable {
