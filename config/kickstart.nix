158552f466e135112cc60a1c0e92de2bf263a379
Justin Hoang <j124.dev@proton.me>
2025-08-22 09:57:59 -0600

    fix(kickstart): remove unnamed register clipboard sync

--- config/kickstart.nix
+++ config/kickstart.nix
@@ -35,9 +35,9 @@
         # xsel.enable = true; # For X11
       };
 
-      # Sync clipboard between OS and Neovim
-      #  Remove this option if you want your OS clipboard to remain independent.
-      register = "unnamedplus";
+      # # Sync clipboard between OS and Neovim
+      # #  Remove this option if you want your OS clipboard to remain independent.
+      # register = "unnamedplus";
     };
 
     # Enable break indent
