7dffde1db0ed3d7a158288328d02d5ef56e75004
Justin Hoang <j124.dev@proton.me>
2025-10-31 13:33:03 -0600

    fix(guess-indent): disable lazy load

--- config/plugins/kickstart/guess-indent.nix
+++ config/plugins/kickstart/guess-indent.nix
@@ -20,18 +20,6 @@ in
     # Detect tabstop and shiftwidth automatically
     plugins.guess-indent = {
       enable = true;
-
-      lazyLoad = lib.mkIf config.plugins.lz-n.enable {
-        enable = true;
-        settings = {
-          # LazyFile is a shorthand that lazy.nvim uses
-          event = [
-            "BufReadPost"
-            "BufWritePost"
-            "BufNewFile"
-          ];
-        };
-      };
     };
   };
 }
