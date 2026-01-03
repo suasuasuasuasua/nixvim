bc7dbbb92d3431cfb3538b96214d0d09a2ca5de4
Justin Hoang <j124.dev@proton.me>
2026-01-03 13:09:51 -0700

    feat(trim): init module

---
+++ config/plugins/custom/trim.nix
@@ -0,0 +1,33 @@
+{
+  lib,
+  config,
+  ...
+}:
+let
+  name = "trim";
+  cfg = config.nixvim.plugins.custom.${name};
+in
+{
+  options.nixvim.plugins.custom.${name} = {
+    enable = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = "Enable ${name} plugin for neovim";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    # https://github.com/cappyzawa/trim.nvim
+    plugins.trim = {
+      enable = true;
+
+      lazyLoad = lib.mkIf config.plugins.lz-n.enable {
+        enable = true;
+
+        settings = {
+          event = [ "DeferredUIEnter" ];
+        };
+      };
+    };
+  };
+}
